dee570d11614ba2584ff8e564ae63736af15085a
lucas <lucasdondertman@gmail.com>
2019-10-16 18:00:13 -0400

    expanded js folder to separate files

--- js/activeheader.js
+++ js/activeheader.js
@@ -18,33 +18,3 @@ function scrollFunction() {
     document.getElementById("topBtn").style.display = "none";
   }
 }
-
-// When the user clicks on the resume button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-//-------------------
-
-var slideIndex = [1,1];
-var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"]
-showSlides(1, 0);
-showSlides(1, 1);
-showSlides(1, 2);
-showSlides(1, 3);
-
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
-}
-
-function showSlides(n, no) {
-  var i;
-  var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  x[slideIndex[no]-1].style.display = "block";
-}
