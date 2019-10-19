eeafb981c534606cc8012f7786adca22a7b5d9e8
lucas <lucasdondertman@gmail.com>
2019-10-19 12:21:06 -0400

    minor style changes + moved button to top to bottom of page

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,23 +1,29 @@
+//add another 1 to slideIndex for each new slideshow
+var slideIndex = [1,1,1,1];
+
+//add name of new slideshow to slideId
+var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"]
+
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[slideIndex[no]-1].style.display = "block";
+}
+
+//this gets called onload, add another for each new slideshow
 function slideshow() {
-  var slideIndex = [1,1];
-  var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"]
   showSlides(1, 0);
   showSlides(1, 1);
   showSlides(1, 2);
   showSlides(1, 3);
-
-  function plusSlides(n, no) {
-    showSlides(slideIndex[no] += n, no);
-  }
-
-  function showSlides(n, no) {
-    var i;
-    var x = document.getElementsByClassName(slideId[no]);
-    if (n > x.length) {slideIndex[no] = 4}
-    if (n < 2) {slideIndex[no] = x.length}
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";
-    }
-    x[slideIndex[no]-1].style.display = "block";
-  }
 }
